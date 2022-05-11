c54e865e734e1dd609007823bf3d0061ac5f0d45
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-05-11 19:03:05 -0400

    start work on questions

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ inquirer = require ("inquirer");
 // usage information, contribution guidelines, and test
 // instructions.  Also ask about license, github username,
 // end -mail address.
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "answer_01",
+        message: "Question 1?",
+        default: "",
+        when: true
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    let prompt = inquirer.createPromptModule();
+    prompt(questions).then(answers => {
+        console.log(answers)});
+}
 
 // Function call to initialize app
 init();
