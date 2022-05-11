92b8a7678a73fed5ef19f934cffdebd1e6a5e2fa
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-05-11 17:32:58 -0400

    include assignment documents

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 /* Professional README Generator */
 
+// TODO: Include packages needed for this application
 inquirer = require ("inquirer");
+
+// TODO: Create an array of questions for user input
+// Ask about title, description, installation instructions,
+// usage information, contribution guidelines, and test
+// instructions.  Also ask about license, github username,
+// end -mail address.
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
 
